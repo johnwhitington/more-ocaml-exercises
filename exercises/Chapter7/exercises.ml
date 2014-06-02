d348ea28717d803f796f1e16080ba0ae7a32670a
John Whitington <john@coherentgraphics.co.uk>
2014-06-02 14:46:22 +0100

    PDF ones in

--- exercises/Chapter7/exercises.ml
+++ exercises/Chapter7/exercises.ml
@@ -1,43 +1,42 @@
-let sum l = List.fold_left ( + ) 0 l
+(* 1 *)
+let make ~len ~elt =
+  Array.make len elt
 
-let maxlist l = List.fold_left max min_int l
+(* 2 *)
+type start = Start of int
 
-let all l = List.fold_left ( && ) true l
+type length = Length of int
 
-let any l = List.fold_left ( || ) false l
+let fill a (Start s) (Length l) v =
+  for x = s to s + l - 1 do a.(x) <- v done
 
-let concat l = List.fold_left ( @ ) [] l
+let filled () =
+  let a = Array.make 100 "x" in
+    fill a (Start 20) (Length 40) "y";
+    a
 
-let rev l = List.fold_left (fun a e -> e :: a) [] l
+(* 3 *)
 
-let member x l = List.fold_left (fun a e -> e = x || a) false l
+(* 4 *)
+let sub b ~off ~len =
+  Buffer.sub b off len
 
-let setify l = List.fold_left (fun a e -> if List.mem e a then a else e :: a) [] l
+let blit src ~srcoff dst ~dstoff ~len =
+  Buffer.blit src srcoff dst dstoff len
 
-type 'a tree =
-    Lf
-  | Br of 'a * 'a tree * 'a tree
+let add_substring b s ~ofs ~len =
+  Buffer.add_substring b s ofs len
 
-let rec fold_tree f e t =
-  match t with
-    Lf -> e
-  | Br (x, l, r) -> f x (fold_tree f e l) (fold_tree f e r)
+(* 5 *)
+let rec map_inner a f l =
+  match l with
+    [] -> List.rev a
+  | h::t -> map_inner (f h :: a) f t
 
-let example =
-  Br (1, Br (2, Lf, Lf), Br (6, Br (4, Lf, Lf), Lf))
+let map f l = map_inner [] f l
 
-let tree_size t = fold_tree (fun _ l r -> 1 + l + r) 0 t
-
-let tree_sum t = fold_tree (fun x l r -> x + l + r) 0 t
-
-let tree_preorder t  = fold_tree (fun x l r -> [x] @ l @ r) [] t
-
-let tree_inorder t   = fold_tree (fun x l r -> l @ [x] @ r) [] t
-
-let tree_postorder t = fold_tree (fun x l r -> l @ r @ [x]) [] t
-
-let map f l = List.fold_right (fun a e -> f e :: a) l
-
-let fold_right f l e =
-  List.fold_left (fun x y -> f y x) e (rev l)
+let rec map ?(a = []) f l =
+  match l with
+    [] -> List.rev a
+  | h::t -> map ~a:(f h :: a) f t
 
