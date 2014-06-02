8f7ea4ac44ed7fb798de93fa4edc193914c6a39d
John Whitington <john@coherentgraphics.co.uk>
2014-06-02 13:52:29 +0100

    First build

---
+++ exercises/Chapter8/exercises.ml
@@ -0,0 +1,33 @@
+(* 1 *)
+let rec cycle_of_points_inner b l =
+  match l with
+    [] ->
+      Buffer.contents b
+  | [(x, y)] ->
+      Printf.bprintf b "(%i, %i)" x y;
+      Buffer.contents b
+  | (x, y)::t ->
+      Printf.bprintf b "(%i, %i) --> " x y;
+      cycle_of_points_inner b t
+
+let cycle_of_points l =
+  match l with
+    [] -> ""
+  | h::t -> cycle_of_points_inner (Buffer.create 256) ((h :: t) @ [h])
+
+(* 2 *)
+let hex_of_string s = 
+  let b = Buffer.create (String.length s * 2) in
+    String.iter
+      (fun c -> Printf.bprintf b "%02X" (int_of_char c))
+      s;
+    Buffer.contents b
+
+(* 3 *)
+let mkstring () = "string"
+
+let _ = Printf.printf "%s" (mkstring ())
+
+(* 4 *)
+let _ = Printf.sprintf "(%*i)" 10 1
+
