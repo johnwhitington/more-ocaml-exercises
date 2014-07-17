ff5d3d61732603c3df83d84a296f30d88be8967b
John Whitington <john@coherentgraphics.co.uk>
2014-07-17 10:36:55 +0100

    Split chapter 6

--- exercises/Chapter6/exercises.ml
+++ exercises/Chapter6/exercises.ml
@@ -1,143 +1,224 @@
-(* Answers for chapter 5 *)
+(* Answers for chapter 6 *)
+open More
+open Input
+open Bits
 
-type input =
-  {pos_in : unit -> int;
-   seek_in : int -> unit;
-   input_char : unit -> char;
-   in_channel_length : int}
+let white_terminating_codes =
+  [|[0; 0; 1; 1; 0; 1; 0; 1]; 
+    [0; 0; 0; 1; 1; 1];
+    [0; 1; 1; 1];
+    [1; 0; 0; 0];
+    [1; 0; 1; 1];
+    [1; 1; 0; 0];
+    [1; 1; 1; 0];
+    [1; 1; 1; 1];
+    [1; 0; 0; 1; 1];
+    [1; 0; 1; 0; 0];
+    [0; 0; 1; 1; 1];
+    [0; 1; 0; 0; 0];
+    [0; 0; 1; 0; 0; 0];
+    [0; 0; 0; 0; 1; 1];
+    [1; 1; 0; 1; 0; 0];
+    [1; 1; 0; 1; 0; 1];
+    [1; 0; 1; 0; 1; 0];
+    [1; 0; 1; 0; 1; 1];
+    [0; 1; 0; 0; 1; 1; 1];
+    [0; 0; 0; 1; 1; 0; 0];
+    [0; 0; 0; 1; 0; 0; 0];
+    [0; 0; 1; 0; 1; 1; 1];
+    [0; 0; 0; 0; 0; 1; 1];
+    [0; 0; 0; 0; 1; 0; 0];
+    [0; 1; 0; 1; 0; 0; 0];
+    [0; 1; 0; 1; 0; 1; 1];
+    [0; 0; 1; 0; 0; 1; 1];
+    [0; 1; 0; 0; 1; 0; 0];
+    [0; 0; 1; 1; 0; 0; 0];
+    [0; 0; 0; 0; 0; 0; 1; 0];
+    [0; 0; 0; 0; 0; 0; 1; 1];
+    [0; 0; 0; 1; 1; 0; 1; 0];
+    [0; 0; 0; 1; 1; 0; 1; 1];
+    [0; 0; 0; 1; 0; 0; 1; 0];
+    [0; 0; 0; 1; 0; 0; 1; 1];
+    [0; 0; 0; 1; 0; 1; 0; 0];
+    [0; 0; 0; 1; 0; 1; 0; 1];
+    [0; 0; 0; 1; 0; 1; 1; 0];
+    [0; 0; 0; 1; 0; 1; 1; 1];
+    [0; 0; 1; 0; 1; 0; 0; 0];
+    [0; 0; 1; 0; 1; 0; 0; 1];
+    [0; 0; 1; 0; 1; 0; 1; 0];
+    [0; 0; 1; 0; 1; 0; 1; 1];
+    [0; 0; 1; 0; 1; 1; 0; 0];
+    [0; 0; 1; 0; 1; 1; 0; 1];
+    [0; 0; 0; 0; 0; 1; 0; 0];
+    [0; 0; 0; 0; 0; 1; 0; 1];
+    [0; 0; 0; 0; 1; 0; 1; 0];
+    [0; 0; 0; 0; 1; 0; 1; 1];
+    [0; 1; 0; 1; 0; 0; 1; 0];
+    [0; 1; 0; 1; 0; 0; 1; 1];
+    [0; 1; 0; 1; 0; 1; 0; 0];
+    [0; 1; 0; 1; 0; 1; 0; 1];
+    [0; 0; 1; 0; 0; 1; 0; 0];
+    [0; 0; 1; 0; 0; 1; 0; 1];
+    [0; 1; 0; 1; 1; 0; 0; 0];
+    [0; 1; 0; 1; 1; 0; 0; 1];
+    [0; 1; 0; 1; 1; 0; 1; 0];
+    [0; 1; 0; 1; 1; 0; 1; 1];
+    [0; 1; 0; 0; 1; 0; 1; 0];
+    [0; 1; 0; 0; 1; 0; 1; 1];
+    [0; 0; 1; 1; 0; 0; 1; 0];
+    [0; 0; 1; 1; 0; 0; 1; 1];
+    [0; 0; 1; 1; 0; 1; 0; 0]|]
 
-type input_bits =
-  {input : input;
-   mutable byte : int;
-   mutable bit : int}
 
-let rec getbit b =
+
+(* QUESTIONS *)
+
+(* 1. Byte-by-byte on lists. *)
+
+(* Compression, not tail-recursive *)
+
+(* Type for runs *)
+type run = Same of int * int | Diff of int list
+
+(* Return list of same characters from a non-null list, and rest *)
+let rec getsame x n l =
+  match l with
+  | h::t when h = x -> getsame x (n + 1) t
+  | _ -> (n, l)
+
+(* Return list of different characters from a non-null list, until two are the
+ * same as one another *)
+let rec getdiff a l =
+  match l with
+    [] -> (List.rev a, [])
+  | h::t ->
+      if a = [] then getdiff [h] t
+      else if h <> List.hd a then getdiff (h :: a) t
+      else (List.rev (List.tl a), List.hd a :: l)
+
+(* Get a single run *)
+let getrun l =
+  match l with
+    [] -> raise (Invalid_argument "getrun")
+  | h::_ ->
+      match getsame h 0 l with
+        1, _ -> let diff, rest = getdiff [] l in (Diff diff, rest)
+      | n, rest -> (Same (n, h), rest)
+
+(* Build the next chars from a run *)
+let chars_of_run r =
+  match r with
+    Same (length, c) -> [257 - length; c]
+  | Diff chars -> List.length chars - 1 :: chars 
+
+(* Compression, tail-recursive *)
+let rec compress_inner a l =
+  match l with
+    [] -> List.concat (List.map chars_of_run (List.rev a))
+  | _ ->
+      let run, rest = getrun l in
+        compress_inner (run :: a) rest
+
+let compress l = compress_inner [] l @ [128]
+
+(* Decompression, tail-recursive *)
+let rec decompress_inner a l =
+  match l with
+    [128] -> List.concat (List.rev a)
+  | [] | [_] -> raise (Invalid_argument "decompress_inner")
+  | h::t::t' ->
+      if h < 127 then
+        let bytes = Util.take (t :: t') (h + 1) in
+        let rest = Util.drop (t :: t') (h + 1) in
+          decompress_inner (bytes :: a) rest
+      else if h > 128 then
+        decompress_inner (Array.to_list (Array.make (257 - h) t) :: a) t' 
+      else decompress_inner a []
+
+let decompress l = decompress_inner [] l
+
+(* 2. Huffman tree from codes. *)
+let from s e =
+  if e < s then raise (Invalid_argument "from") else
+    let n = ref [] in
+      for x = s to e do n := x :: !n done;
+      List.rev !n
+
+type tree = Lf | Code of int | Br of tree * tree
+
+let rec add_elt tr (l, n) =
+  match l with
+    0::m ->
+      begin match tr with
+        Lf -> Br (add_elt Lf (m, n), Lf)
+      | Br (left, right) -> Br (add_elt left (m, n), right)
+      | Code x -> raise (Failure "collision")
+      end
+  | 1::m ->
+      begin match tr with
+        Lf -> Br (Lf, add_elt Lf (m, n))
+      | Br (left, right) -> Br (left, add_elt right (m, n))
+      | Code x -> raise (Failure "collision")
+      end
+  | [] -> Code n
+  | _ -> raise (Failure "bad code")
+
+let make_tree arr numbers =
+  List.fold_left
+    add_elt
+    Lf
+    (List.combine (Array.to_list arr) numbers)
+
+let white_terminating_tree =
+  make_tree
+    white_terminating_codes
+    (from 0 (Array.length white_terminating_codes - 1))
+
+(* 4. Making a histogram of black and white frequencies. This is a pair of int
+ * arrays of length 1792 for run lengths. *)
+let getbitint b =
+  if getbit b then 1 else 0
+  
+let peekbit b =
   if b.bit = 0 then
     begin
-      b.byte <- int_of_char (b.input.input_char ());
-      b.bit <- 128;
-      getbit b
+      let byte = int_of_char (b.input.input_char ()) in
+        rewind b.input;
+        byte land 128 > 0
     end
   else
-    let r = b.byte land b.bit > 0 in
-      b.bit <- b.bit / 2;
-      r
-     
-let align b =
-  b.bit <- 0
+    b.byte land b.bit > 0
 
-let getval b n =
-  if n <= 0 || n > 31 then
-    raise (Invalid_argument "getval")
-  else
-    let r = ref 0 in
-      for x = n - 1 downto 0 do
-        r := !r lor ((if getbit b then 1 else 0) lsl x)
-      done;
-      !r
+let rec read_up_to v i n w =
+  if n >= w then (n, v) else
+    match peekbit i with
+      x when x = v -> (ignore (getbit i)); read_up_to v i (n + 1) w
+    | x -> (n, v)
 
-(* 1 *)
-let getval_fast b n =
-  if n = 8 && b.bit = 0
-    then int_of_char (b.input.input_char ())
-    else getval b n
+let build_histogram a_white a_black i w h =
+  let toread = ref (w * h) in
+  let wleft = ref w in
+    while !toread > 0 do
+      let n, v = read_up_to (peekbit i) i 0 !wleft in
+        let a = if v then a_black else a_white in
+          a.(n) <- a.(n) + 1;
+          toread := !toread - n;
+          wleft := !wleft - n;
+          if !wleft = 0 then wleft := w
+    done
 
-(* 2 *)
-let getval_32 b n =
-  if n < 0 then raise (Invalid_argument "getval_32") else
-    if n = 0 then 0l else
-      let r = ref Int32.zero in
-        for x = n - 1 downto 0 do
-          r := Int32.logor !r (Int32.shift_left (Int32.of_int ((if getbit b then 1 else 0))) x)
-        done;
-        !r
+let histogram_of_input i w h =
+  let white = Array.make 1792 0 in
+  let black = Array.make 1792 0 in
+    build_histogram white black (input_bits_of_input i) w h;
+    (white, black)
 
-(* 3 *)
-type output =
-  {output_char : char -> unit;
-   out_channel_length : unit -> int}
+let print_histogram =
+  Array.iteri
+    (fun x n -> if n > 0 then Printf.printf "%i runs of length %i\n" n x)
 
-type output_bits =
-  {output : output; (* underlying output *)
-   mutable obyte : int; (* the byte we're building up *)
-   mutable obit : int}
+let test i w h =
+  histogram_of_input i w h
 
-let flush o =
-  if o.obit < 7 then o.output.output_char (char_of_int o.obyte);
-  o.obyte <- 0;
-  o.obit <- 7
 
-let rec putbit o b =
-  if o.obit = -1 then
-    begin
-      flush o;
-      putbit o b
-    end
-  else
-    begin
-      if b = 1 then o.obyte <- o.obyte lor (1 lsl o.obit);
-      o.obit <- o.obit - 1
-    end
-
-let putval o v l =
-  for x = l - 1 downto 0 do
-    putbit o (v land (1 lsl x))
-  done
-
-let putval_fast o v l =
-  if l = 8 && o.obit = 7
-    then o.output.output_char (char_of_int v)
-    else putval o v l
-    
-(* 4 FIXME *)
-let putval_32 o v l =
-  for x = l - 1 downto 0 do
-    putbit o (Int32.to_int (Int32.logand v (Int32.shift_left (Int32.of_int x) 1)))
-  done
-
-(* 5 *)
-type output2 =
-  {output_char : char -> unit;
-   rewind : unit -> unit;
-   out_channel_length : unit -> int}
-
-let output_of_string s =
-  let pos = ref 0 in
-    {output_char =
-       (fun c ->
-          if !pos < String.length s
-            then (s.[!pos] <- c; pos := !pos + 1)
-            else raise End_of_file);
-     rewind =
-       (fun () ->
-          if !pos > 0
-            then pos := !pos - 1
-            else raise (Failure "rewind"));
-     out_channel_length =
-       (fun () -> String.length s)}
-
-type output_bits2 =
-  {output : output2; 
-   mutable obyte : int;
-   mutable obit : int}
-
-let output_bits_of_output output =
-  {output;
-   obyte = 0;
-   obit = 0}
-  
-let rec putbit o b =
-  if o.obit = -1 then
-    begin
-      o.obyte <- 0;
-      o.obit <- 7;
-      putbit o b
-    end
-  else
-    begin
-      if b = 1 then o.obyte <- o.obyte lor (1 lsl o.obit);
-      o.output.output_char (char_of_int o.obyte);
-      o.output.rewind ();
-      o.obit <- o.obit - 1
-    end
 
